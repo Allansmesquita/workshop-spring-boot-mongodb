d9b899045ddd20434874f42857b5db880d1308a7
Allan Mesquita <allan_santoz@hotmail.com>
2023-08-01 00:11:07 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/allansmesquita/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.allansmesquita.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.allansmesquita.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> { 
+
+}
