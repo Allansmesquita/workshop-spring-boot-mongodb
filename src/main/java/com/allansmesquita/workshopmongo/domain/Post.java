e76c64921ee2189889519b900b57ed372047543e
Allan Mesquita <allan_santoz@hotmail.com>
2023-08-02 20:47:55 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/allansmesquita/workshopmongo/domain/Post.java
+++ src/main/java/com/allansmesquita/workshopmongo/domain/Post.java
@@ -1,25 +1,30 @@
 package com.allansmesquita.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.allansmesquita.workshopmongo.dto.AuthorDTO;
+import com.allansmesquita.workshopmongo.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
 	}
 
@@ -70,8 +75,16 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
-	}	
-	
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,7 +102,4 @@ public class Post implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
-	
 }
