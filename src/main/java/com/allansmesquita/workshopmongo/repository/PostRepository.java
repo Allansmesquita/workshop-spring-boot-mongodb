e741dc14ef29e807981b00a00fa5e162d0f4932d
Allan Mesquita <allan_santoz@hotmail.com>
2023-08-02 15:03:33 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/allansmesquita/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.allansmesquita.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.allansmesquita.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> { 
+
+}
